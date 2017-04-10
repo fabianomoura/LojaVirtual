dda029b065cec0a1d993d50c15be9acc4c61cefb
Fabiano Moura <fabiano@fabianomoura.com.br>
2017-04-10 17:13:22 -0300

    Terceiro commit

---
+++ Quiron.LojaVirtual.Web/Controllers/LojaController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Quiron.LojaVirtual.Web.Controllers
+{
+    public class LojaController : Controller
+    {
+        // GET: Loja
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
