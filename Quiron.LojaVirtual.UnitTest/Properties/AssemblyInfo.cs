9ab9f51ffe93bfc70bc1a43322488ee037c17623
Fabiano Moura <fabiano@fabianomoura.com.br>
2017-04-10 17:10:19 -0300

    Segundo commit

---
+++ Quiron.LojaVirtual.UnitTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Quiron.LojaVirtual.UnitTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Quiron.LojaVirtual.UnitTest")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("53c8b52f-5598-4da8-9082-45146f85bf94")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Quiron.LojaVirtual.UnitTest/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Quiron.LojaVirtual.UnitTest
+{
+    [TestClass]
+    public class QuironUnitTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
